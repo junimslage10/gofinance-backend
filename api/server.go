4341bc439bb36ae2f5b58321532d46244c619dea
junimslage10 <junimslage@gmail.com>
2022-08-29 19:40:34 -0300

    Create server gin

--- api/server.go
+++ api/server.go
@@ -1,6 +1,28 @@
 package api
 
-// import (
-// 	"github.com/gin-goinc/gin"
-// 	db "github.com/junimslage10/gofinance-backend/db/sqlc"
-// )
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/junimslage10/gofinance-backend/db/sqlc"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func newServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	server.router = router
+
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"Api as error": err.Error()}
+}
