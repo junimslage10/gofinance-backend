08dcb79fcee801be6bad601e372896918d16940e
junimslage10 <junimslage@gmail.com>
2022-08-29 21:28:53 -0300

    server: update server

--- api/server.go
+++ api/server.go
@@ -10,7 +10,7 @@ type Server struct {
 	router *gin.Engine
 }
 
-func newServer(store *db.SQLStore) *Server {
+func NewServer(store *db.SQLStore) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
